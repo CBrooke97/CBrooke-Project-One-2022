bddf8e08a3b86643baa80e5887739b82717ee57f
CBrooke97 <carterbrooke@ntlworld.com>
2022-04-12 22:12:36 +0100

    Started refactor of Stats code

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+    [SerializeField] private int level;
+    [SerializeField] private int currentExp;
+    [SerializeField] private int maxExp;
+
+    [SerializeField] private float currentHealth;
+    [SerializeField] private float maxHealth;
+
+    public float CurrentHealth
+    {
+        get
+        { return currentHealth; }
+    }
+    public float MaxHealth { get; set; }
+
+
+    [SerializeField] private float currentAstralus;
+    [SerializeField] private float maxAstralus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float value)
+    {
+        if(currentHealth - value <= 0)
+        {
+            //Death code here
+        }
+        else
+        {
+            currentHealth -= value;
+        }
+    }
+
+    public void Heal(float value)
+    {
+        if (currentHealth + value >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += value;
+        }
+    }
+}
